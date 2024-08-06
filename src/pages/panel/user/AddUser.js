bd523e569c3a5beb586981f5330a3621e5cad2bb
amriarendy <amriarendy90@gmail.com>
2024-08-07 00:12:29 +0700

    complete upload file

--- src/pages/panel/user/AddUser.js
+++ src/pages/panel/user/AddUser.js
@@ -20,85 +20,85 @@ const AddUser = () => {
     ],
   };
   const navigate = useNavigate();
-  // const [name, setName] = useState("");
-  // const [email, setEmail] = useState("");
-  // const [password, setPassword] = useState("");
-  // const [dob, setDob] = useState("");
-  // const [phone, setPhone] = useState("");
-  // const [gender, setGender] = useState("");
-  // const [photo, setPhoto] = useState("");
-  // const [biography, setBiography] = useState("");
-  // const [status, setStatus] = useState("Active");
-  // const [position, setPosition] = useState("");
-  // const [country, setCountry] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [dob, setDob] = useState("");
+  const [phone, setPhone] = useState("");
+  const [gender, setGender] = useState("");
+  const [photo, setPhoto] = useState("");
+  const [biography, setBiography] = useState("");
+  const [status, setStatus] = useState("Active");
+  const [position, setPosition] = useState("");
+  const [country, setCountry] = useState("");
 
-  // const handleSubmit = async (e) => {
-  //   e.preventDefault();
-  //   const formData = new FormData();
-  //   formData.append("name", name);
-  //   formData.append("email", email);
-  //   formData.append("password", password);
-  //   formData.append("dob", dob);
-  //   formData.append("phone", phone);
-  //   formData.append("gender", gender);
-  //   if (photo) {
-  //     formData.append("photo", photo); // Pastikan photo adalah objek File
-  //   }
-  //   formData.append("biography", biography);
-  //   formData.append("status", status);
-  //   formData.append("position", position);
-  //   formData.append("country", country);
-  //   console.log("formData: ", formData);
-  //   try {
-  //     await axios.post("http://localhost:3001/users", formData, {
-  //       headers: {
-  //         "Content-type": "multipart/form-data",
-  //       },
-  //     });
-  //     navigate("/user");
-  //   } catch (error) {
-  //     if (error.response) {
-  //       // Request made and server responded with a status code outside of 2xx
-  //       console.error("Server responded with an error:", error.response.data);
-  //       console.error("Server status code:", error.response.status);
-  //       console.error("Server headers:", error.response.headers);
-  //     } else if (error.request) {
-  //       // Request was made but no response received
-  //       console.error("No response received:", error.request);
-  //     } else {
-  //       // Something else caused the error
-  //       console.error("Error message:", error.message);
-  //     }
-  //   }
-  // };
-
-  const { values, handleChange, setValues } = useForm(
-    {
-      id: 1,
-      email: "",
-      password: "",
-      name: "",
-      dob: "",
-      phone: "",
-      gender: "",
-      photo: "",
-      biography: "",
-      status: "active",
-      position: "",
-      country: "",
-    },
-    handleSubmit
-  );
-
-  async function handleSubmit(e) {
+  const handleSubmit = async (e) => {
     e.preventDefault();
+    const formData = new FormData();
+    formData.append("name", name);
+    formData.append("email", email);
+    formData.append("password", password);
+    formData.append("dob", dob);
+    formData.append("phone", phone);
+    formData.append("gender", gender);
+    if (photo) {
+      formData.append("photo", photo); // Pastikan photo adalah objek File
+    }
+    formData.append("biography", biography);
+    formData.append("status", status);
+    formData.append("position", position);
+    formData.append("country", country);
+    console.log("formData: ", formData);
     try {
-      await store("users", values);
+      await axios.post("http://localhost:3001/users", formData, {
+        headers: {
+          "Content-type": "multipart/form-data",
+        },
+      });
       navigate("/user");
     } catch (error) {
-      console.error(error);
+      if (error.response) {
+        // Request made and server responded with a status code outside of 2xx
+        console.error("Server responded with an error:", error.response.data);
+        console.error("Server status code:", error.response.status);
+        console.error("Server headers:", error.response.headers);
+      } else if (error.request) {
+        // Request was made but no response received
+        console.error("No response received:", error.request);
+      } else {
+        // Something else caused the error
+        console.error("Error message:", error.message);
+      }
     }
-  }
+  };
+
+  // const { values, handleChange, setValues } = useForm(
+  //   {
+  //     id: 1,
+  //     email: "",
+  //     password: "",
+  //     name: "",
+  //     dob: "",
+  //     phone: "",
+  //     gender: "",
+  //     photo: "",
+  //     biography: "",
+  //     status: "active",
+  //     position: "",
+  //     country: "",
+  //   },
+  //   handleSubmit
+  // );
+
+  // async function handleSubmit(e) {
+  //   e.preventDefault();
+  //   try {
+  //     await store("users", values);
+  //     navigate("/user");
+  //   } catch (error) {
+  //     console.error(error);
+  //   }
+  // }
 
   return (
     <>
