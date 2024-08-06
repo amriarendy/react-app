c0d755a139fcd2b7e1feb44aa2ea3780d5bee883
amriarendy <amriarendy90@gmail.com>
2024-08-07 00:13:03 +0700

    fix create and upload file

--- app/controllers/UserController.js
+++ app/controllers/UserController.js
@@ -25,15 +25,17 @@ export const getWhere = async (req, res) => {
 };
 
 export const store = async (req, res) => {
-  // res.status(200).json({ status: "success", message: "Data created" })
-  if(req.files === null) return res.status(400).json({ status: "error", message: "Invalid images" });
-  const photo = req.files.photo;
-  const name = req.body.name;
+  if (req.files === null)
+    return res
+      .status(400)
+      .json({ code: 400, status: "error", message: "Invalid image" });
   const email = req.body.email;
   const password = req.body.password;
+  const name = req.body.name;
   const dob = req.body.dob;
-  const phone = req.body.phone;
   const gender = req.body.gender;
+  const photo = req.files.photo;
+  const phone = req.body.phone;
   const biography = req.body.biography;
   const status = req.body.status;
   const position = req.body.position;
@@ -42,20 +44,46 @@ export const store = async (req, res) => {
   const fileSize = photo.data.length;
   const ext = path.extname(photo.name);
   const fileName = photo.md5 + ext;
-  // const url = `${req.protocol}://${req.get("host")}/uploads/profile/${fileName}`;
-  const allowedType = ['.png','.jpg','.jpeg'];
+  const urlPhoto = `${req.protocol}://${req.get(
+    "host"
+  )}/uploads/profile/${fileName}`;
+  const allowedType = [".png", ".jpg", ".jpeg"];
 
-  if(!allowedType.includes(ext.toLowerCase())) return res.status(422).json({message: "Invalid Images"});
-  if(fileSize > 5000000) return res.status(422).json({message: "Image must be less than 5 MB"});
-  photo.mv(`./public/uploads/profile/${fileName}`, async(err)=>{
-    if(err) return res.status(500).json({message: err.message});
-  try {
-    await User.create({email: email, password: password, name: name, dob: dob, phone: phone, gender: gender, biography: biography, status: status, position: position, country: country, photo: fileName});
-    res.status(201).json({ status: "success", message: "Data created" });
-  } catch (error) {
-    console.log(error.message);
-  }
-});
+  if (!allowedType.includes(ext.toLowerCase()))
+    return res
+      .status(422)
+      .json({ code: 422, status: "error", message: "Invalid image" });
+  if (fileSize > 5000000)
+    return res.status(422).json({ message: "Image must be less than 5 MB" });
+  photo.mv(`./public/uploads/profile/${fileName}`, async (err) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ code: 500, status: "error", message: err.message });
+    try {
+      await User.create({
+        email: email,
+        password: password,
+        name: name,
+        dob: dob,
+        phone: phone,
+        gender: gender,
+        biography: biography,
+        status: status,
+        position: position,
+        country: country,
+        photo: urlPhoto,
+      });
+      res
+        .status(201)
+        .json({ code: 201, status: "success", message: "Data insert" });
+    } catch (error) {
+      console.log(error.message);
+      return res
+        .status(500)
+        .json({ code: 500, status: "error", message: error.message });
+    }
+  });
 };
 
 export const update = async (req, res) => {
