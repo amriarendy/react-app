130ba8e23d3ae458be6881f132d54ebe8ad3b75f
amriarendy <amriarendy90@gmail.com>
2024-08-26 23:56:09 +0700

    add middleware and depedencies dotenv

--- app/controllers/AuthController.js
+++ app/controllers/AuthController.js
@@ -1,17 +1,57 @@
 import Auth from "../models/AuthModel.js";
-import { Jwt } from "jsonwebtoken";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 
 export const signIn = async (req, res) => {
   try {
     const user = await Auth.findAll({
-      where:{
-        email: req.body.email
-      }
+      where: {
+        email: req.body.email,
+      },
     });
-    const match = await bcrypt.compare
+    const match = await bcrypt.compare(req.body.password, user[0].password);
+    console.log(process.env.ACCESS_TOKEN_SECRET);
+    if (!match)
+      return res
+        .status(400)
+        .json({ code: 400, status: "error", message: "Incorect password!" });
+    const userId = user[0].id;
+    const name = user[0].name;
+    const email = user[0].email;
+    const tokenAccess = jwt.sign(
+      { userId, name, email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expireIn: "20s",
+      }
+    );
+    const tokenRefresh = jwt.sign(
+      { userId, name, email },
+      process.env.ACCESS_TOKEN_SECRET,
+      {
+        expireIn: "1d",
+      }
+    );
+    await Auth.update(
+      { refresh_token: tokenRefresh },
+      {
+        where: {
+          id: userId,
+        },
+      }
+    );
+    res.cookie("tokenRefresh", tokenRefresh, {
+      httpOnly: true,
+      maxAge: 24 * 60 * 60 * 1000,
+    });
+    res.json({ tokenAccess });
   } catch (error) {
     console.log(error.message);
-    res.status(404).json({ code: 404, status: "error", message: "Email or password not found!" })
+    res.status(404).json({
+      code: 404,
+      status: "error",
+      message: "Email or password not found!",
+    });
   }
 };
 
@@ -24,9 +64,10 @@ export const signOut = async (req, res) => {
 };
 
 export const register = async (req, res) => {
+  const salt = await bcrypt.genSalt();
   const name = req.body.name;
   const email = req.body.email;
-  const password = req.body.password;
+  const password = await bcrypt.hash(req.body.password, salt);
   const status = "Active";
   const photo = "default.png";
   const urlPhoto = `${req.protocol}://${req.get(
--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import express from "express";
 import FileUpload from "express-fileupload";
 import cors from "cors";
+import dotenv from "dotenv";
 import UserRoute from "./app/routes/UserRoute.js";
 import TagRoute from "./app/routes/TagRoute.js";
 import CategoryRoute from "./app/routes/CategoryRoute.js";
 import BlogRoute from "./app/routes/BlogRoute.js";
 import AuthRoute from "./app/routes/AuthRoute.js";
 
+dotenv.config();
 const app = express();
 const port = 3001;
 app.use(cors());
