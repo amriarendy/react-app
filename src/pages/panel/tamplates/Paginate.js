42bdc7060b32c4413b7e03a400582b431efe4852
amriarendy <amriarendy90@gmail.com>
2025-07-25 08:38:07 +0700

    create paginate number at components

--- src/pages/panel/tamplates/Paginate.js
+++ src/pages/panel/tamplates/Paginate.js
@@ -1,5 +1,7 @@
 import PanelLayout from "../PanelLayout";
 import Breadcrumbs from "../../../components/molecules/breadcrumbs/Breadcrumbs";
+import { IoIosArrowBack } from "react-icons/io";
+import { IoIosArrowForward } from "react-icons/io";
 
 const Paginate = () => {
   const breadCrumbs = {
@@ -14,20 +16,102 @@ const Paginate = () => {
       <PanelLayout>
         <Breadcrumbs breadCrumbs={breadCrumbs} />
         <div className="grid grid-cols-1 px-4 pt-6 lg:grid-cols-1 gap-4 mb-4 dark:bg-gray-900">
-          <h1 className="text-center mb-4 text-4xl font-extrabold leading-none tracking-tight text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-            Write Your Holy Content Here!!
-          </h1>
-          <p className="text-justify mb-6 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry. Lorem Ipsum has been the industry's standard dummy text
-            ever since the 1500s, when an unknown printer took a galley of type
-            and scrambled it to make a type specimen book. It has survived not
-            only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
-          </p>
+          <div className="p-4 mb-4 space-y-4 bg-white border border-gray-200 rounded-lg shadow-sm 2xl:col-span-2 dark:border-gray-700 sm:p-6 dark:bg-gray-800">
+            <h3 className="text-xl font-semibold dark:text-white">Paginate</h3>
+            <div className={`grid grid-cols-full gap-6`}>
+              <div className="flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6">
+                <div className="flex flex-1 justify-between sm:hidden">
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Previous
+                  </a>
+                  <a
+                    href="#"
+                    className="relative ml-3 inline-flex items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
+                  >
+                    Next
+                  </a>
+                </div>
+                <div className="hidden sm:flex sm:flex-1 sm:items-center sm:justify-between">
+                  <div>
+                    <p className="text-sm text-gray-700">
+                      Showing <span className="font-medium">1</span> to{" "}
+                      <span className="font-medium">10</span> of{" "}
+                      <span className="font-medium">97</span> results
+                    </p>
+                  </div>
+                  <div>
+                    <nav
+                      aria-label="Pagination"
+                      className="isolate inline-flex -space-x-px rounded-md shadow-xs"
+                    >
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Previous</span>
+                        <IoIosArrowBack aria-hidden="true" className="size-5" />
+                      </a>
+                      {/* Current: "z-10 bg-indigo-600 text-white focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" */}
+                      <a
+                        href="#"
+                        aria-current="page"
+                        className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                      >
+                        1
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        2
+                      </a>
+                      <a
+                        href="#"
+                        className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                      >
+                        3
+                      </a>
+                      <span className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-gray-300 ring-inset focus:outline-offset-0">
+                        ...
+                      </span>
+                      <a
+                        href="#"
+                        className="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex"
+                      >
+                        8
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        9
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        10
+                      </a>
+                      <a
+                        href="#"
+                        className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-gray-300 ring-inset hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                      >
+                        <span className="sr-only">Next</span>
+                        <IoIosArrowForward
+                          aria-hidden="true"
+                          className="size-5"
+                        />
+                      </a>
+                    </nav>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <h3 className="text-xl font-semibold dark:text-white">footer</h3>
+          </div>
         </div>
       </PanelLayout>
     </>
