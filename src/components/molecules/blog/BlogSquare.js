5de387f6e283c9708b8319973e35af094fb05b95
amriarendy <amriarendy90@gmail.com>
2025-07-31 15:21:51 +0700

    fixing blog view in welcome page

--- src/components/molecules/blog/BlogSquare.js
+++ src/components/molecules/blog/BlogSquare.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-const BlogSquare = () => {
+const BlogSquare = ({ titleOnly }) => {
   return (
     <div className="rounded-lg border border-gray-200 bg-white p-6 shadow-sm dark:border-gray-700 dark:bg-gray-800">
       <div className="h-56 w-full">
@@ -27,21 +27,27 @@ const BlogSquare = () => {
         >
           Apple iMac 27", 1TB HDD, Retina 5K Display, M3 Max
         </a>
-        <p className="text-gray-600 mt-2">
-          Over the past year, Volosoft has undergone many changes! After months
-          of preparation and some hard work, we moved to our new office.
-        </p>
-        <div className="flex items-center mt-4">
-          <img
-            src="https://placehold.co/32x32"
-            alt="Profile picture of Jese Leos"
-            className="w-8 h-8 rounded-full"
-          />
-          <div className="ml-2">
-            <p className="text-gray-800 text-sm font-semibold">Jese Leos</p>
-            <p className="text-gray-600 text-sm">Aug 15, 2021 · 16 min read</p>
-          </div>
-        </div>
+        {titleOnly ? (
+          <></> // or some title-only JSX here
+        ) : (
+          <>
+            <p className="text-gray-600 mt-2">
+              Over the past year, Volosoft has undergone many changes! After months
+              of preparation and some hard work, we moved to our new office.
+            </p>
+            <div className="flex items-center mt-4">
+              <img
+                src="https://placehold.co/32x32"
+                alt="Profile picture of Jese Leos"
+                className="w-8 h-8 rounded-full"
+              />
+              <div className="ml-2">
+                <p className="text-gray-800 text-sm font-semibold">Jese Leos</p>
+                <p className="text-gray-600 text-sm">Aug 15, 2021 · 16 min read</p>
+              </div>
+            </div>
+          </>
+        )}
       </div>
     </div>
   );
